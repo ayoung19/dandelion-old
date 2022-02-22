7708f045ea5a28fc4678745e2b68834fd3ac8f7c
ayoung <zeusandninja@gmail.com>
2022-02-21 23:57:26 -0800

    Starter frontend with elastic ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "@elastic/eui/dist/eui_theme_light.css";
+
+import { EuiProvider } from "@elastic/eui";
+import { App } from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <EuiProvider colorMode="light">
+      <App />
+    </EuiProvider>
+  </StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
